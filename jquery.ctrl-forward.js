966ec399528ea0d7c497088adb00232030975165
Bremen Braun <bremen.braun@gmail.com>
2013-11-04 15:55:49 -0700

    Fixed jQuery scoping bug in click context

--- jquery.ctrl-forward.js
+++ jquery.ctrl-forward.js
@@ -57,8 +57,9 @@
 			}
         
 		$this.click(function() {
+			var $this = $(this),
 			var action = parseURL($this.attr(settings['fwd-attr'])),
-			    form = $(this).parents('form');
+			    form = $this.parents('form');
 			
 			form.get(0).setAttribute('action', action);
 		});
